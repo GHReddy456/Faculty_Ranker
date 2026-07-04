3bb1261b810a5ca7e36a1a4d3700a746336b7a32
Harig <harig@example.com>
2026-07-05 04:31:36 +0530

    Add sitemap and robots

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://faculty-ranker-sand.vercel.app/sitemap.xml',
+  }
+}
